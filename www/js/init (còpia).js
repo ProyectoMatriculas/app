b0d9c3b8c5574bceeb623e2dc0fdee7474172aa9
OscarD01 <oscar-dolo@hotmail.com>
2021-03-22 18:37:37 +0100

    Spec 7

--- www/js/init (còpia).js
+++ www/js/init (còpia).js
@@ -6,7 +6,18 @@
     $('.sidenav').sidenav();
     $('.tabs').tabs({ "swipeable": true });
 
-     
+     $(document).ready(function() {
+            $('.fixed-action-btn').floatingActionButton();
+        });
+
+        if ($(".carousel").length > 0) {
+            $(".carousel")[0].style = "height: 100%;";
+        }
+
+        $(document).ready(function() {
+            $('.tap-target').tapTarget();
+            $('.tap-target').tapTarget('open');
+        });
  
   }); // end of document ready
 })(jQuery); // end of jQuery name space
