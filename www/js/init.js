1e0558068afb2c57d26e029e959a3636961f2895
OscarD01 <oscar-dolo@hotmail.com>
2021-03-22 16:07:42 +0100

    Materialize + template

---
+++ www/js/init.js
@@ -0,0 +1,19 @@
+// Arxiu init.js + index.js
+ 
+(function($){
+  $(function(){
+ 
+    $('.sidenav').sidenav();
+ 
+  }); // end of document ready
+})(jQuery); // end of jQuery name space
+ 
+ 
+document.addEventListener('deviceready', onDeviceReady, false);
+ 
+function onDeviceReady() {
+    // Cordova is now initialized. Have fun!
+ 
+    console.log('Running cordova-' + cordova.platformId + '@' + cordova.version);
+    //document.getElementById('deviceready').classList.add('ready');
+}
