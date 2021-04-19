7a565f12d2ebf6950bbe5ca9be146b61f08b5d37
imkrysis <elkrysss@gmail.com>
2021-04-19 18:37:58 +0200

    Get UFS by CourseCode

--- www/js/index.js
+++ www/js/index.js
@@ -27,3 +27,100 @@ function onDeviceReady() {
     console.log('Running cordova-' + cordova.platformId + '@' + cordova.version);
     document.getElementById('deviceready').classList.add('ready');
 }
+
+function getUfs() {
+    let query = "/cycles/read?course_code=" + courseCode;
+    $.ajax({
+        method: "GET",
+        url: "https://morning-peak-25234.herokuapp.com" + query,
+        dataType: "json",
+    }).done(function(cycle) {
+        fillUfList(cycle);
+    }).fail(function() {
+        alert("Ha habido un error.");
+    });
+
+}
+
+function saveUfsList() {
+    let query = "/student/import/ufs?email=" + user.email + "&json=" + ufsList;
+
+    $.ajax({
+        method: "GET",
+        url: "https://morning-peak-25234.herokuapp.com" + query,
+        dataType: "text",
+    }).done(function() {
+        alert("UFs guardadas correctamente");
+    }).fail(function() {
+        alert("Ha habido un error.");
+    });
+}
+
+
+function showUfs(cycle) {
+
+	cycle[0].modules.sort();
+    cicle[0].modules.forEach(m => {
+        addCollapsibleLi(m.module_code, m.module_name);
+        m.ufs.forEach(uf => {
+            addCheckbox(m.codi_modul, u.uf_name);
+        });
+    });
+
+	checkboxList = $(".selected");
+
+    checkboxList.click(function() {
+        isChecked = false;
+        for (let i = 1; i < checkboxList.length; i++) {
+            if (checkboxList[i].checked) {
+                isChecked = true;
+            } else {
+                isChecked = false;
+                break;
+            }
+        }
+        
+        if (isChecked) {
+            selectAll.checked = true;
+        } else {
+            selectAll.checked = false;
+        }
+    });
+}
+
+function addCollapsibleLi(id, content) {
+    $("#collapsibleContainer").append('<li><div class="collapsible-header"><i class="material-icons">more_horiz</i>' + content + '</div><div id="' + id + '" class="collapsible-body custom-padding-top-bottom-1rem"></div></li>');
+}
+
+function addCheckbox(id, content) {
+    $("#" + id).append('<p><label><input id="' + content + '" name="UF-chk" type="checkbox" class="filled-in waves-effect waves-grey grey darken-3 orange-text" checked="false"/><span>' + content + '</span></label></p>');
+}
+
+function toggleCheckbox(id) {
+    document.getElementById(id).toggleAttribute("checked");
+}
+
+function selectAllCheckbox() {
+    $(".selected").prop('checked', "checked");
+}
+
+function unselectAllCheckbox() {
+    $(".selected").prop('checked', false);
+}
+
+function getSelectedCheckbox() {
+    let checkedList = $('input[name="UF-chk"]:checked');
+    let arrayUF = []
+    
+    if (checkedList.length > 0) {
+        arrayUF = [];
+        for (let i = 0; i < checkedList.length; i++) {
+            arrayUF.push(checkedList[i].id.toString());
+        }
+        console.log(arrayUF);
+        return JSON.stringify(arrayUF);
+    }
+
+    return [];
+}
+
